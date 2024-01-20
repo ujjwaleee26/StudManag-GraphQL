6de20bc036e7d595a91fc15a38fcf2001c345603
Ujjwal Sinha <sinhaujjwal04@gmail.com>
2024-01-20 22:08:39 +0530

    add

--- src/main/java/com/springboot/GraphQL/Controller/Query.java
+++ src/main/java/com/springboot/GraphQL/Controller/Query.java
@@ -1,5 +1,6 @@
 package com.springboot.GraphQL.Controller;
 
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -9,5 +10,10 @@ public class Query
    @QueryMapping	
    public String firstQuery() {
 	   return "First GQL Query";
+   }
+   
+   @QueryMapping
+   public String secondQuery(@Argument String firstName,@Argument String lastName) {
+	   return firstName+" "+lastName;
    }
 }
